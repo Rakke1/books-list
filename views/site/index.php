20d8f507943957c89110530f97d75f90cfa516f3
Ivan Tag <vanyatag95@gmail.com>
2024-10-09 23:59:39 +0500

    init commit

---
+++ views/site/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Home page';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+        <h1 class="display-4">Books list</h1>
+    </div>
+</div>
